7ad1b3d6c83495c14f42dab9c210a2ee812396a0
Stefan Duprey <stefan.duprey@yahoo.fr>
2014-12-02 14:08:18 +0100

    propre files output for title crawler

--- SERP_ENGINE/src/proxy/testing/ProxyTesting.java
+++ SERP_ENGINE/src/proxy/testing/ProxyTesting.java
@@ -1,9 +1,7 @@
 package proxy.testing;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
--- TITLE_DUPLICATION/sql_scripts/selecting_scripts.sql
+++ TITLE_DUPLICATION/sql_scripts/selecting_scripts.sql
@@ -1,14 +1,14 @@
-select * from duplicates order by nb_urls desc
+select * from current_duplicates order by nb_urls desc
 
 
 # duplication par magasins
-select magazin, sum(nb_urls) as global_count from duplicates group by magazin order by global_count desc
+select magazin, sum(nb_urls) as global_count from current_duplicates group by magazin order by global_count desc
 
 # duplication par rayon
-select rayon,sum(nb_urls) as global_count from duplicates group by rayon order by global_count desc
+select rayon,sum(nb_urls) as global_count from current_duplicates group by rayon order by global_count desc
 
 # duplication par produit
-select product, sum(nb_urls) as global_count from duplicates where product not like '' group by product order by global_count desc
+select product, sum(nb_urls) as global_count from current_duplicates where product not like '' group by product order by global_count desc
 
 # duplication par produit avec rayon
 select product_aggregate.product, product_aggregate.global_count, referential.rayon from product_aggregate, referential where referential.product=product_aggregate.product
