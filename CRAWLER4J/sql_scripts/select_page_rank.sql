df41c26de2817bc948d6e8f738768f90f175e57e
Stefan Duprey <stefan.duprey@yahoo.fr>
2015-02-09 14:08:41 +0100

    page rank sql queries

--- CRAWLER4J/sql_scripts/select_page_rank.sql
+++ CRAWLER4J/sql_scripts/select_page_rank.sql
@@ -1,10 +1,10 @@
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' order by page_rank desc limit 100;
 
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' and depth=1 order by page_rank desc limit 100;
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' and depth=2 order by page_rank desc limit 100;
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' and depth=3 order by page_rank desc limit 100;
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' and depth=4 order by page_rank desc limit 100;
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' and depth=5 order by page_rank desc limit 100;
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' and depth=6 order by page_rank desc limit 100;
-select url, page_type, magasin, produit, page_rank from crawl_results where magasin = 'maison' and depth=7 order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' and depth=1 order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' and depth=2 order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' and depth=3 order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' and depth=4 order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' and depth=5 order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' and depth=6 order by page_rank desc limit 100;
+select url, page_type, magasin, produit as rayon, page_rank from crawl_results where magasin = 'maison' and depth=7 order by page_rank desc limit 100;
 
