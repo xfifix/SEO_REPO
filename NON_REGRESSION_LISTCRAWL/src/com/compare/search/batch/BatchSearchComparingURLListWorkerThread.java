ba39441d47e1f25e610e1c76bb3326bccbfef022
Stefan Duprey <stefan.duprey@yahoo.fr>
2015-04-28 17:44:50 +0200

    non reg search dex

--- NON_REGRESSION_LISTCRAWL/src/com/compare/search/batch/BatchSearchComparingURLListWorkerThread.java
+++ NON_REGRESSION_LISTCRAWL/src/com/compare/search/batch/BatchSearchComparingURLListWorkerThread.java
@@ -26,7 +26,7 @@ import com.parsing.utility.FacettesUtility;
 
 public class BatchSearchComparingURLListWorkerThread implements Runnable {
 	private static int batch_size = 100;
-	private static String updateStatement ="UPDATE SOLR_VS_EXALEAD_SEARCH_LIST SET STATUS=?, H1_SOLR=?, TITLE_SOLR=?, XPATH1_SOLR=?, XPATH2_SOLR=?, XPATH3_SOLR=?, XPATH4_SOLR=?, XPATH5_SOLR=?, XPATH6_SOLR=?, XPATH7_SOLR=?, XPATH8_SOLR=?, XPATH9_SOLR=?, XPATH10_SOLR=?, H1_EXALEAD=?, TITLE_EXALEAD=?, XPATH1_EXALEAD=?, XPATH2_EXALEAD=?, XPATH3_EXALEAD=?, XPATH4_EXALEAD=?, XPATH5_EXALEAD=?, XPATH6_EXALEAD=?, XPATH7_EXALEAD=?, XPATH8_EXALEAD=?, XPATH9_EXALEAD=?, XPATH10_EXALEAD=?, H1_COMPARISON=?, TITLE_COMPARISON=?, XPATH1_COMPARISON=?, XPATH2_COMPARISON=?, XPATH3_COMPARISON=?, XPATH4_COMPARISON=?, XPATH5_COMPARISON=?, XPATH6_COMPARISON=?, XPATH7_COMPARISON=?, XPATH8_COMPARISON=?, XPATH9_COMPARISON=?, XPATH10_COMPARISON=?, TO_FETCH=FALSE WHERE ID=?";
+	private static String updateStatement ="UPDATE SOLR_VS_EXALEAD_SEARCH_LIST SET STATUS_SOLR=?, NB_PRODUCTS_SOLR=?, FACETTES_SOLR=?,NB_PRODUCTS_EXALEAD=?, FACETTES_EXALEAD=?, TO_FETCH=FALSE WHERE ID=?";
 	private String user_agent;
 	private List<ExpressionId> my_expressions_to_fetch = new ArrayList<ExpressionId>();
 	private Connection con;
@@ -98,235 +98,42 @@ public class BatchSearchComparingURLListWorkerThread implements Runnable {
 
 	// batched update
 	private void updateStatus(List<FacettesURLComparisonInfo> infos){
-		//		System.out.println("Adding to batch : " + infos.size() + "ULRs into database");
-		//		try {
-		//			//Statement st = con.createStatement();
-		//			con.setAutoCommit(false); 
-		//			PreparedStatement st = con.prepareStatement(updateStatement);
-		//			for (int i=0;i<infos.size();i++){
-		//				URLComparisonInfo local_info = infos.get(i);
-		//				ParsingOutput solrOutput = local_info.getSolrOutput();
-		//
-		//				String H1_SOLR = solrOutput.getH1().replace("'", "");
-		//				String TITLE_SOLR = solrOutput.getTitle().replace("'", "");
-		//				String[] XPATHRESULTS_SOLR = solrOutput.getXpathResults();
-		//
-		//				ParsingOutput exaleadOutput = local_info.getExaleadOutput();			
-		//				String H1_EXALEAD = exaleadOutput.getH1().replace("'", "");
-		//				String TITLE_EXALEAD = exaleadOutput.getTitle().replace("'", "");
-		//				String[] XPATHRESULTS_EXALEAD = exaleadOutput.getXpathResults();
-		//
-		//				st.setInt(1, local_info.getStatus());
-		//				st.setString(2,H1_SOLR);
-		//				st.setString(3, TITLE_SOLR);
-		//				if (XPATHRESULTS_SOLR != null){	
-		//					if (XPATHRESULTS_SOLR[0] != null){
-		//						st.setString(4, XPATHRESULTS_SOLR[0]);
-		//					} else {
-		//						st.setString(4, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[1] != null){
-		//						st.setString(5, XPATHRESULTS_SOLR[1]);
-		//					} else {
-		//						st.setString(5, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[2] != null){
-		//						st.setString(6, XPATHRESULTS_SOLR[2]);
-		//					} else {
-		//						st.setString(6, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[3] != null){
-		//						st.setString(7, XPATHRESULTS_SOLR[3]);
-		//					} else {
-		//						st.setString(7, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[4] != null){
-		//						st.setString(8, XPATHRESULTS_SOLR[4]);
-		//					} else {
-		//						st.setString(8, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[5] != null){
-		//						st.setString(9, XPATHRESULTS_SOLR[5]);
-		//					} else {
-		//						st.setString(9, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[6] != null){
-		//						st.setString(10, XPATHRESULTS_SOLR[6]);
-		//					} else {
-		//						st.setString(10, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[7] != null){
-		//						st.setString(11, XPATHRESULTS_SOLR[7]);
-		//					} else {
-		//						st.setString(11, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[8] != null){
-		//						st.setString(12, XPATHRESULTS_SOLR[8]);
-		//					} else {
-		//						st.setString(12, "");
-		//					}
-		//					if (XPATHRESULTS_SOLR[9] != null){
-		//						st.setString(13, XPATHRESULTS_SOLR[9]);
-		//					} else {
-		//						st.setString(13, "");
-		//					}
-		//				}else {
-		//					st.setString(4, "");
-		//					st.setString(5, "");
-		//					st.setString(6, "");
-		//					st.setString(7, "");
-		//					st.setString(8, "");
-		//					st.setString(9, "");
-		//					st.setString(10, "");
-		//					st.setString(11, "");
-		//					st.setString(12, "");
-		//					st.setString(13, "");
-		//				}
-		//				st.setString(14,H1_EXALEAD);
-		//				st.setString(15, TITLE_EXALEAD);
-		//				if (XPATHRESULTS_EXALEAD != null){
-		//
-		//					if (XPATHRESULTS_EXALEAD[0] != null){
-		//						st.setString(16, XPATHRESULTS_EXALEAD[0]);
-		//					} else {
-		//						st.setString(16, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[1] != null){
-		//						st.setString(17, XPATHRESULTS_EXALEAD[1]);
-		//					} else {
-		//						st.setString(17, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[2] != null){
-		//						st.setString(18, XPATHRESULTS_EXALEAD[2]);
-		//					} else {
-		//						st.setString(18, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[3] != null){
-		//						st.setString(19, XPATHRESULTS_EXALEAD[3]);
-		//					} else {
-		//						st.setString(19, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[4] != null){
-		//						st.setString(20, XPATHRESULTS_EXALEAD[4]);
-		//					} else {
-		//						st.setString(20, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[5] != null){
-		//						st.setString(21, XPATHRESULTS_EXALEAD[5]);
-		//					} else {
-		//						st.setString(21, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[6] != null){
-		//						st.setString(22, XPATHRESULTS_EXALEAD[6]);
-		//					} else {
-		//						st.setString(22, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[7] != null){
-		//						st.setString(23, XPATHRESULTS_EXALEAD[7]);
-		//					} else {
-		//						st.setString(23, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[8] != null){
-		//						st.setString(24, XPATHRESULTS_EXALEAD[8]);
-		//					} else {
-		//						st.setString(24, "");
-		//					}
-		//					if (XPATHRESULTS_EXALEAD[9] != null){
-		//						st.setString(25, XPATHRESULTS_EXALEAD[9]);
-		//					} else {
-		//						st.setString(25, "");
-		//					}
-		//				}else {
-		//					st.setString(16, "");
-		//					st.setString(17, "");
-		//					st.setString(18, "");
-		//					st.setString(19, "");
-		//					st.setString(20, "");
-		//					st.setString(21, "");
-		//					st.setString(22, "");
-		//					st.setString(23, "");
-		//					st.setString(24, "");
-		//					st.setString(25, "");
-		//				}
-		//				if (!H1_SOLR.equals(H1_EXALEAD)){
-		//					st.setInt(26, 0);
-		//				} else {
-		//					st.setInt(26, 1);
-		//				}
-		//				if (!TITLE_SOLR.equals(TITLE_EXALEAD)){
-		//					st.setInt(27, 0);
-		//				} else {
-		//					st.setInt(27, 1);
-		//				}
-		//
-		//				if ((XPATHRESULTS_EXALEAD != null) && (XPATHRESULTS_SOLR != null)){
-		//					if (!XPATHRESULTS_SOLR[0].equals(XPATHRESULTS_EXALEAD[0])){
-		//						st.setInt(28, 0);
-		//					} else {
-		//						st.setInt(28, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[1].equals(XPATHRESULTS_EXALEAD[1])){
-		//						st.setInt(29, 0);
-		//					} else {
-		//						st.setInt(29, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[2].equals(XPATHRESULTS_EXALEAD[2])){
-		//						st.setInt(30, 0);
-		//					} else {
-		//						st.setInt(30, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[3].equals(XPATHRESULTS_EXALEAD[3])){
-		//						st.setInt(31, 0);
-		//					} else {
-		//						st.setInt(31, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[4].equals(XPATHRESULTS_EXALEAD[4])){
-		//						st.setInt(32, 0);
-		//					} else {
-		//						st.setInt(32, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[5].equals(XPATHRESULTS_EXALEAD[5])){
-		//						st.setInt(33, 0);
-		//					} else {
-		//						st.setInt(33, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[6].equals(XPATHRESULTS_EXALEAD[6])){
-		//						st.setInt(34, 0);
-		//					} else {
-		//						st.setInt(34, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[7].equals(XPATHRESULTS_EXALEAD[7])){
-		//						st.setInt(35, 0);
-		//					} else {
-		//						st.setInt(35, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[8].equals(XPATHRESULTS_EXALEAD[8])){
-		//						st.setInt(36, 0);
-		//					} else {
-		//						st.setInt(36, 1);
-		//					}
-		//					if (!XPATHRESULTS_SOLR[9].equals(XPATHRESULTS_EXALEAD[9])){
-		//						st.setInt(37, 0);
-		//					} else {
-		//						st.setInt(37, 1);
-		//					}
-		//				}
-		//
-		//				st.setInt(38, local_info.getId());
-		//				//UPDATE HTTPINFOS_LIST SET STATUS=?, H1=?, TITLE=?, XPATH1=?, XPATH2=?, XPATH3=?, XPATH4=?, XPATH5=?, TO_FETCH=FALSE WHERE ID=?";
-		//				//	String batch ="UPDATE HTTPINFOS_LIST SET STATUS="+infos.get(i).getStatus()+", H1='"+H1+"', TITLE='"+TITLE+ "',TO_FETCH=FALSE WHERE ID="+infos.get(i).getId();
-		//				st.addBatch();		
-		//			}      
-		//			//int counts[] = st.executeBatch();
-		//			System.out.println("Beginning to insert : " + infos.size() + "ULRs into database");
-		//			st.executeBatch();
-		//			con.commit();
-		//			st.close();
-		//			System.out.println("Having inserted : " + infos.size() + "ULRs into database");
-		//		} catch (SQLException e){
-		//			e.printStackTrace();
-		//			System.out.println("Trouble inserting batch ");
-		//		}
+		System.out.println("Adding to batch : " + infos.size() + "ULRs into database");
+		try {
+			//Statement st = con.createStatement();
+			con.setAutoCommit(false); 
+			PreparedStatement st = con.prepareStatement(updateStatement);
+			for (int i=0;i<infos.size();i++){
+				FacettesURLComparisonInfo local_info = infos.get(i);
+				FacettesParsingOutput solrOutput = local_info.getSolrOutput();
+
+				String FACETTES_SOLR = solrOutput.getFacettes();
+				String NBPRODUCTS_SOLR = solrOutput.getNb_products();
+				
+				FacettesParsingOutput exaleadOutput = local_info.getExaleadOutput();		
+				String FACETTES_EXALEAD = exaleadOutput.getFacettes();
+				String NBPRODUCTS_EXALEAD = exaleadOutput.getNb_products();
+				
+				//UPDATE SOLR_VS_EXALEAD_SEARCH_LIST SET STATUS_SOLR=?, NB_PRODUCTS_SOLR=?, FACETTES_SOLR=?,NB_PRODUCTS_EXALEAD=?, FACETTES_EXALEAD=?, TO_FETCH=FALSE WHERE ID=?";
+
+				st.setInt(1, local_info.getStatus());
+				st.setString(2, NBPRODUCTS_SOLR);
+				st.setString(3, FACETTES_SOLR);
+				st.setString(4, NBPRODUCTS_EXALEAD);
+				st.setString(5, FACETTES_EXALEAD);
+				st.setInt(6, local_info.getId());
+				st.addBatch();		
+			}      
+			//int counts[] = st.executeBatch();
+			System.out.println("Beginning to insert : " + infos.size() + "ULRs into database");
+			st.executeBatch();
+			con.commit();
+			st.close();
+			System.out.println("Having inserted : " + infos.size() + "ULRs into database");
+		} catch (SQLException e){
+			e.printStackTrace();
+			System.out.println("Trouble inserting batch ");
+		}
 	}
 
 	private String construct_solr_url_search(ExpressionId expression){
